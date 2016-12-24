01841ed1365a34dcb7a53458d7ea5c5297693957
JuLima14 <julian-lima@live.com>
2016-12-24 10:50:16 -0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -20,7 +20,20 @@ var IncidentesController = require('../app/controllers/IncidenteController');
 // esto nos permite obtener data con un POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+//==============================================================================
+var pg = require('pg');
 
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 // ROUTES para la api API
 // =============================================================================
