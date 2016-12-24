4f0173da3a193ce000848725879e5673d0e81024
JuLima14 <julian-lima@live.com>
2016-12-24 11:43:13 -0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -12,7 +12,7 @@ var bodyParser = require('body-parser');
 var methodOverride  = require("method-override");
 var ip = require("ip");
 var pg = require("pg");
-var DATABASE_URL = "postgres://twlfziyretvaol:e47476a474996d6288a1cedf20448b60e1352f27783af0d506d2d589746805c2@ec2-54-235-112-37.compute-1.amazonaws.com:5432/d7t6cddl7ofpsl?ssl=true";
+var DATABASE_URL1 = "postgres://twlfziyretvaol:e47476a474996d6288a1cedf20448b60e1352f27783af0d506d2d589746805c2@ec2-54-235-112-37.compute-1.amazonaws.com:5432/d7t6cddl7ofpsl?ssl=true";
 var port = process.env.PORT || 8080;        // set our port
 var IncidentesController = require('../app/controllers/IncidenteController');
 
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 
 
 pg.defaults.ssl = true;
-pg.connect(DATABASE_URL, function(err, client) {
+pg.connect(DATABASE_URL1, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
 /*
