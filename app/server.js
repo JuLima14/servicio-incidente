39daed09eb904bd78845a0679a6df1c1f17d75b2
JuLima14 <julian-lima@live.com>
2016-12-24 11:24:42 -0300

    Update server.js

--- app/server.js
+++ app/server.js
@@ -27,12 +27,12 @@ pg.defaults.ssl = true;
 pg.connect(process.env.DATABASE_URL1, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');
-
+/*
   client
     .query('SELECT table_schema,table_name FROM information_schema.tables;')
     .on('row', function(row) {
       console.log(JSON.stringify(row));
-    });
+    });*/
 });
 
 // ROUTES para la api API
